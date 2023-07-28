6ea4fef575234ea6b1d1ffd65beb91004f890d50
isd-sgcu <isd.team.sgcu@gmail.com>
2023-07-28 16:54:06 +0000

    Update from https://github.com/isd-sgcu/rpkm66-proto/commit/c5ffc6f7815ca406986f57dd1fde4b852bdf261c

--- rpkm66/checkin/staff/v1/staff_grpc.pb.go
+++ rpkm66/checkin/staff/v1/staff_grpc.pb.go
@@ -21,6 +21,7 @@ const _ = grpc.SupportPackageIsVersion7
 const (
 	StaffService_AddEventToUser_FullMethodName      = "/rpkm66.checkin.staff.v1.StaffService/AddEventToUser"
 	StaffService_GenerateSignInToken_FullMethodName = "/rpkm66.checkin.staff.v1.StaffService/GenerateSignInToken"
+	StaffService_IsStaff_FullMethodName             = "/rpkm66.checkin.staff.v1.StaffService/IsStaff"
 )
 
 // StaffServiceClient is the client API for StaffService service.
@@ -29,6 +30,7 @@ const (
 type StaffServiceClient interface {
 	AddEventToUser(ctx context.Context, in *AddEventToUserRequest, opts ...grpc.CallOption) (*AddEventToUserResponse, error)
 	GenerateSignInToken(ctx context.Context, in *GenerateSignInTokenRequest, opts ...grpc.CallOption) (*GenerateSignInTokenResponse, error)
+	IsStaff(ctx context.Context, in *IsStaffRequest, opts ...grpc.CallOption) (*IsStaffResponse, error)
 }
 
 type staffServiceClient struct {
@@ -57,12 +59,22 @@ func (c *staffServiceClient) GenerateSignInToken(ctx context.Context, in *Genera
 	return out, nil
 }
 
+func (c *staffServiceClient) IsStaff(ctx context.Context, in *IsStaffRequest, opts ...grpc.CallOption) (*IsStaffResponse, error) {
+	out := new(IsStaffResponse)
+	err := c.cc.Invoke(ctx, StaffService_IsStaff_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // StaffServiceServer is the server API for StaffService service.
 // All implementations must embed UnimplementedStaffServiceServer
 // for forward compatibility
 type StaffServiceServer interface {
 	AddEventToUser(context.Context, *AddEventToUserRequest) (*AddEventToUserResponse, error)
 	GenerateSignInToken(context.Context, *GenerateSignInTokenRequest) (*GenerateSignInTokenResponse, error)
+	IsStaff(context.Context, *IsStaffRequest) (*IsStaffResponse, error)
 	mustEmbedUnimplementedStaffServiceServer()
 }
 
@@ -75,6 +87,9 @@ func (UnimplementedStaffServiceServer) AddEventToUser(context.Context, *AddEvent
 }
 func (UnimplementedStaffServiceServer) GenerateSignInToken(context.Context, *GenerateSignInTokenRequest) (*GenerateSignInTokenResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GenerateSignInToken not implemented")
+}
+func (UnimplementedStaffServiceServer) IsStaff(context.Context, *IsStaffRequest) (*IsStaffResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsStaff not implemented")
 }
 func (UnimplementedStaffServiceServer) mustEmbedUnimplementedStaffServiceServer() {}
 
@@ -125,6 +140,24 @@ func _StaffService_GenerateSignInToken_Handler(srv interface{}, ctx context.Cont
 	return interceptor(ctx, in, info, handler)
 }
 
+func _StaffService_IsStaff_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IsStaffRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(StaffServiceServer).IsStaff(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: StaffService_IsStaff_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(StaffServiceServer).IsStaff(ctx, req.(*IsStaffRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // StaffService_ServiceDesc is the grpc.ServiceDesc for StaffService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -139,6 +172,10 @@ var StaffService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GenerateSignInToken",
 			Handler:    _StaffService_GenerateSignInToken_Handler,
+		},
+		{
+			MethodName: "IsStaff",
+			Handler:    _StaffService_IsStaff_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
